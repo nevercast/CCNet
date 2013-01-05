a954e26282754960028dd0aed2e08d8324e46c16
NeverCast <j.nevercast@gmail.com>
2013-01-06 09:51:43 +1300

    Initial commit with update scripts

---
+++ client/update.ps1
@@ -0,0 +1,62 @@
+$name = ""
+$version = "0.0.0.0"
+$incrementBuild = $TRUE
+
+if(Test-Path "VERSION") {
+	write-host "Reading Version info..."
+	$lines = (Get-Content ".\VERSION")[0..1]
+	$name = $lines[0]
+	$version = $lines[1]	
+}
+
+$versionParts = $version.split(".")
+[int]$major = $versionParts[0]
+[int]$minor = $versionParts[1]
+[int]$revision = $versionParts[2]
+[int]$build = $versionParts[3]
+
+if( $incrementBuild ) {
+	$build = $build + 1
+}
+
+$query = read-host "Software Name? [$name]?"
+if( $query -ne "" ) {
+	$name = $query
+	write-host "Name: $name"
+}
+$query = read-host "Major Version? [$major]?"
+if( $query -ne "" ) {
+	$major = $query
+}
+$query = read-host "Minor Version? [$minor]?"
+if( $query -ne "" ) {
+	$minor = $query
+}
+$query = read-host "Revision Number? [$revision]?"
+if( $query -ne "" ) {
+	$revision = $query
+}
+$query = read-host "Build Number? [$build]?"
+if( $query -ne "" ) {
+	$build = $query
+}
+
+$version = "$major.$minor.$revision.$build"
+write-host "Version: $version"
+
+write-host "Writing VERSION..."
+$versionFileOutput = @($name, $version)
+$versionFileOutput | out-file ".\VERSION"
+write-host "Okay"
+
+write-host "Building Index..."
+$workingDirectory = (Get-Location)
+$files = [System.IO.Directory]::GetFiles($workingDirectory, "*.lua", [System.IO.SearchOption]::AllDirectories)
+[Array]$relativeFiles = @()
+Foreach ($file in $files) {
+	$relativeFiles += $file.Replace($workingDirectory, "")
+}
+write-host "Writing Index..."
+$relativeFiles | out-file ".\FILES"
+write-host "Update of $name complete!"
+
---
+++ update.ps1
@@ -0,0 +1,42 @@
+write-host "Starting Global Update"
+$softwarePackages = [System.IO.Directory]::GetDirectories((Get-Location))
+$validPackages = @()
+ForEach ($package in $softwarePackages) {
+	if(Test-Path "$package\VERSION") {
+		$validPackages += $package.Replace((Get-Location), "")
+	}
+}
+$count = $validPackages.Length
+write-host "$count packages found... running updates"
+$validPackages | out-file "PACKAGES"
+
+ForEach($package in $validPackages) {
+	$workingDirectory = (Get-Location).Path + $package
+	$packageName = (Get-Content "$workingDirectory\VERSION")[0]
+	write-host "Checking $packageName in $workingDirectory for changes..."
+	$files = [System.IO.Directory]::GetFiles($workingDirectory, "*.lua", [System.IO.SearchOption]::AllDirectories)
+	[Array]$relativeFiles = @()
+	Foreach ($file in $files) {
+		$relativeFiles += $file.Replace($workingDirectory, "")
+	}	
+	[Array]$compare = (Get-Content "$workingDirectory\FILES")
+	if($compare -eq $null){
+		$compare = @()
+	}
+	Push-Location
+	Set-Location $workingDirectory
+	if($compare.Length -ne $relativeFiles.Length){
+		write-host "File count different, updating $packageName"
+		. "./update"
+	}else{
+		write-host "Files have changed"
+		for($i = 0; $i -lt $compare.Length; $i++) {
+			if($compare[$i] -ne $relativeFiles[$i]){
+				. "./update"
+				break
+			}
+		}
+	}
+	Pop-Location
+}
+write-host "Package update complete!"
